b604fbefc48123a41bdb84019e53060680a7bf89
MonikaSonali <monikasonali96@gmail.com>
2021-10-12 11:31:53 +0530

    minor routing bug fix

--- app.py
+++ app.py
@@ -2,14 +2,6 @@ from flask import Flask, redirect, url_for, render_template, request
 import pandas as pd
 app = Flask(__name__)
 
-# df = pd.read_csv("dump.csv")
-# df = df[df['id'] != 'id']
-# df.reset_index(inplace=True)
-# df.drop(['index'], axis=1, inplace=True)
-# df.sort_values(['observationDateTime'], ascending=False, inplace=True)
-# df = df.drop_duplicates()
-# df.reset_index(inplace=True)
-# df.drop(['index'], axis=1, inplace=True)
 
 @app.route('/')
 def landingPage():
@@ -28,47 +20,33 @@ def getDF():
     # return df.to_html(classes="table table-striped")
     return df
 
-@app.route('/cases')
-def casesHandled(licensePlate):
+@app.route('/cases/<licensePlate>')
+def cases(licensePlate):
 
-    return licensePlate
+    df = getDF()
+    df5 = df[df['license_plate'] == licensePlate].sort_values(by=['observationDateTime'])
+    df5['Counter'] = 0
+    df5.reset_index(inplace=True)
+    df5.drop(['index'], axis=1, inplace=True)
+    indices = []
+    for ind in df5.index:
+        indices.append(ind)
+    for ind in range(0, len(indices)-1):
+        if df5['serviceOnDuty'].iloc[ind] == "YES" and df5['serviceOnDuty'].iloc[ind+1] == "NO":
+               df5['Counter'].iloc[ind] += 1
 
-    print("licensePlate --- ", licensePlate)
-    # df = pd.read_csv("dump.csv")
-    # df = df[df['id'] != 'id']
-    # df.reset_index(inplace=True)
-    # df.drop(['index'], axis=1, inplace=True)
-    # df.sort_values(['observationDateTime'], ascending=False, inplace=True)
-    # df = df.drop_duplicates()
-    # df.reset_index(inplace=True)
-    # df.drop(['index'], axis=1, inplace=True)
-    # df5 = df[df['license_plate'] == licensePlate].sort_values(by=['observationDateTime'])
-    # df5['Counter'] = 0
-    # df5.reset_index(inplace=True)
-    # df5.drop(['index'], axis=1, inplace=True)
-    # indices = []
-    # for ind in df5.index:
-    #     indices.append(ind)
-    # for ind in range(0, len(indices)-1):
-    #     if df5['serviceOnDuty'].iloc[ind] == "YES" and df5['serviceOnDuty'].iloc[ind+1] == "NO":
-    #            df5['Counter'].iloc[ind] += 1
-    #
-    # return render_template('result.html', lp=df5['license_plate'].iloc[0], cases=df5['Counter'].sum())
-    # return df5['license_plate'].iloc[0], df5['Counter'].sum()
+    return render_template('result.html', lp=df5['license_plate'].iloc[0], cases=df5['Counter'].sum())
+    return df5['license_plate'].iloc[0], df5['Counter'].sum()
 
 
 @app.route('/getData', methods=['POST', 'GET'])
 def submit():
 
-       # casesHandled(df,)
-
        if request.method == 'POST':
            licensePlateNumber = request.form['lpnumber']
-           print("REQUEST --- ", request.form['lpnumber'])
-
-
-       # return redirect('https://www.google.com/')
-       return redirect(url_for('casesHandled',  licensePlate=request.form['lpnumber']))
+           print("REQUEST --- ", request.form['lpnumber'], request.args.get('lpnumber'))
+           
+       return redirect(url_for('cases', licensePlate=licensePlateNumber))
 
 if __name__ == '__main__':
     app.run(debug=True)
